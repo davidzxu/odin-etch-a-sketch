979ac46c906c57baf4cfdc470fce61e1f987ec6b
David Xu <171293428+davidzxu@users.noreply.github.com>
2024-07-29 15:52:31 -0400

    Add hover function to grid to change colors between cyan and red

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function createGrid() {
     for (let numColumns = 0; numColumns < 16; numColumns++) {
         const columns = document.createElement("div");
-        columns.setAttribute("class", "grid");
-        columns.textContent = "";
-        for (let numRows = 0; numRows < 15; numRows++) {
+        columns.setAttribute("class", "invisible-grid");
+        for (let numRows = 0; numRows < 16; numRows++) {
             const rows = document.createElement("div");
-            rows.setAttribute("class", "grid");
-            rows.textContent = "";
+            rows.setAttribute("class", "default-grid");
+            rows.addEventListener("mousemove", () => {
+                rows.removeAttribute("class");
+                rows.setAttribute("class", "new-grid");
+            });
             columns.appendChild(rows);
         }
         container.appendChild(columns);
