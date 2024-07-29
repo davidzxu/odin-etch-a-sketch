c2ed00d6a8de2d5fef260e9db00fdc0c8fffbd95
David Xu <171293428+davidzxu@users.noreply.github.com>
2024-07-29 14:16:23 -0400

    Add borders to grids

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function createGrid() {
     for (let numColumns = 0; numColumns < 16; numColumns++) {
         const columns = document.createElement("div");
         columns.setAttribute("class", "grid");
-        columns.textContent = "0";
+        columns.textContent = "";
         for (let numRows = 0; numRows < 15; numRows++) {
             const rows = document.createElement("div");
             rows.setAttribute("class", "grid");
-            rows.textContent = "0";
+            rows.textContent = "";
             columns.appendChild(rows);
         }
         container.appendChild(columns);
